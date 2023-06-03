a5a5d6de921d66e28592b79fa9dc534304eb20ed
clement.l <maple3312936@outlook.com>
2023-06-03 18:22:38 +0800

    test: add tests

--- section3/CompoundPractice/README.md
+++ section3/CompoundPractice/README.md
@@ -3,6 +3,7 @@
 Welcome to the Compound Practice repository! The main purpose of this repository is to provide a platform for everyone to practice using Compound Finance and gain familiarity with its operational principles.
 
 ## Exercise 1
+
 Practice calling `mint` and `redeem` on cTokens, and simulate the accrual of interest on deposited funds over a period of time using Foundry's cheatcode.
 
 ## Environment Setup
@@ -10,21 +11,25 @@ Practice calling `mint` and `redeem` on cTokens, and simulate the accrual of int
 To get started with the Compound Practice repository, follow the steps below:
 
 1. Clone the repository:
+
    ```shell
-   git clone git@github.com:AppWorks-School/Blockchain-Resource.git
+   git clone git@github.com:Doge-is-Dope/Blockchain-Resource-Updated.git
    ```
 
 2. Navigate to the Compound Practice directory:
+
    ```shell
    cd Blockchain-Resource/section3/CompoundPractice
    ```
 
 3. Install the necessary dependencies:
+
    ```shell
    forge install
    ```
 
 4. Build the project:
+
    ```shell
    forge build
    ```
--- section3/CompoundPractice/test/CompoundPractice.t.sol
+++ section3/CompoundPractice/test/CompoundPractice.t.sol
@@ -1,55 +1,96 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity 0.8.19;
+pragma solidity ^0.8.19;
 
-import { EIP20Interface } from "compound-protocol/contracts/EIP20Interface.sol";
-import { CErc20 } from "compound-protocol/contracts/CErc20.sol";
+import {EIP20Interface} from "compound-protocol/contracts/EIP20Interface.sol";
+import {CErc20} from "compound-protocol/contracts/CErc20.sol";
 import "test/helper/CompoundPracticeSetUp.sol";
 
 interface IBorrower {
-  function borrow() external;
+    function borrow() external;
 }
 
 contract CompoundPracticeTest is CompoundPracticeSetUp {
-  EIP20Interface public USDC = EIP20Interface(0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48);
-  CErc20 public cUSDC = CErc20(0x39AA39c021dfbaE8faC545936693aC917d5E7563);
-  address public user;
+    EIP20Interface public USDC = EIP20Interface(0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48);
+    CErc20 public cUSDC = CErc20(0x39AA39c021dfbaE8faC545936693aC917d5E7563);
+    address public user;
 
-  IBorrower public borrower;
+    IBorrower public borrower;
 
-  function setUp() public override {
-    super.setUp();
+    uint256 mainnetFork;
 
-    // Deployed in CompoundPracticeSetUp helper
-    borrower = IBorrower(borrowerAddress);
+    function setUp() public override {
+        // Fork mainnet
+        mainnetFork = vm.createFork("https://eth-mainnet.g.alchemy.com/v2/dwhCNmMVbzaUyIBpd6IhRppq_ZB01_JI");
+        vm.selectFork(mainnetFork);
 
-    user = makeAddr("User");  
+        super.setUp();
 
-    uint256 initialBalance = 10000 * 10 ** USDC.decimals();
-    deal(address(USDC), user, initialBalance);
+        // Deployed in CompoundPracticeSetUp helper
+        borrower = IBorrower(borrowerAddress);
 
-    vm.label(address(cUSDC), "cUSDC");
-    vm.label(borrowerAddress, "Borrower");
-  }
+        user = makeAddr("User");
 
-  function test_compound_mint_interest() public {
-    vm.startPrank(user); 
-    // TODO: 1. Mint some cUSDC with USDC
+        uint256 initialBalance = 10000 * 10 ** USDC.decimals();
+        deal(address(USDC), user, initialBalance);
 
-    // TODO: 2. Modify block state to generate interest
+        vm.label(address(cUSDC), "cUSDC");
+        vm.label(borrowerAddress, "Borrower");
+    }
 
-    // TODO: 3. Redeem and check the redeemed amount
-  }
+    function test_compound_mint_interest() public {
+        vm.startPrank(user);
 
-  function test_compound_mint_interest_with_borrower() public {
-    vm.startPrank(user); 
-    // TODO: 1. Mint some cUSDC with USDC
+        uint256 initialBalance = USDC.balanceOf(address(user));
 
-    // 2. Borrower.borrow() will borrow some USDC
-    borrower.borrow();
+        // Approve USDC to cUSDC (error code: 12)
+        USDC.approve(address(cUSDC), initialBalance);
 
-    // TODO: 3. Modify block state to generate interest
+        // TODO: 1. Mint some cUSDC with USDC
+        uint256 mintResult = cUSDC.mint(initialBalance);
 
+        // Assert the mint is successful
+        assertEq(mintResult, 0);
 
-    // TODO: 4. Redeem and check the redeemed amount
-  }
+        // TODO: 2. Modify block state to generate interest
+        vm.roll(block.number + 10000);
+
+        // TODO: 3. Redeem and check the redeemed amount
+        uint256 redeemResult = cUSDC.redeem(cUSDC.balanceOf(address(user)));
+
+        // Assert the redeem is successful
+        assertEq(redeemResult, 0);
+
+        // Redeemed USDC is greater than the initial balance
+        assertGt(USDC.balanceOf(address(user)), initialBalance);
+
+        console.log("Initial balance: %s", initialBalance);
+        console.log("Final balance:   %s", USDC.balanceOf(address(user)));
+    }
+
+    function test_compound_mint_interest_with_borrower() public {
+        vm.startPrank(user);
+
+        uint256 initialBalance = USDC.balanceOf(address(user));
+
+        // Approve USDC to cUSDC (error code: 12)
+        USDC.approve(address(cUSDC), initialBalance);
+
+        // TODO: 1. Mint some cUSDC with USDC
+        assertEq(cUSDC.mint(initialBalance), 0);
+
+        // TODO: 2. Borrower.borrow() will borrow some USDC
+        borrower.borrow();
+
+        // TODO: 3. Modify block state to generate interest
+        vm.roll(block.number + 1000);
+
+        // TODO: 4. Redeem and check the redeemed amount
+        assertEq(cUSDC.redeem(cUSDC.balanceOf(address(user))), 0);
+
+        // Redeemed USDC is greater than the initial balance
+        assertGt(USDC.balanceOf(address(user)), initialBalance);
+
+        console.log("Initial balance: %s", initialBalance);
+        console.log("Final balance:   %s", USDC.balanceOf(address(user)));
+    }
 }
